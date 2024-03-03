db4a17728a545cd6e1122eb733a9b39ab527a4b5
aderahmadw <aderahmad555@gmail.com>
2024-03-03 12:45:36 +0700

    Updt: Setup ovride BS color variable

--- src/js/index.js
+++ src/js/index.js
@@ -11,20 +11,20 @@ import * as bootstrap from "bootstrap";
 
 // const detectRoute = () => routes[window.location.pathname];
 
-// const initPages = () => {
-//   const header = document.querySelector("header");
-//   const main = document.querySelector("main");
-//   const footer = document.querySelector("footer");
+const initPages = () => {
+  const header = document.querySelector("header");
+  const main = document.querySelector("main");
+  const footer = document.querySelector("footer");
 
-//   if (header && main && footer) {
-//     main.style.minHeight = `calc(100vh - ${
-//       header.clientHeight + footer.clientHeight
-//     }px)`;
-//   }
-// };
+  if (header && main && footer) {
+    main.style.minHeight = `calc(100vh - ${
+      header.clientHeight + footer.clientHeight
+    }px)`;
+  }
+};
 
 window.addEventListener("DOMContentLoaded", async () => {
-  // initPages();
+  initPages();
   //   const route = detectRoute();
   //   route.init();
 });
